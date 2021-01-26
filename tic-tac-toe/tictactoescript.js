ea37de9e3f85a63ed46ec159b616183e6389beae
hfg-nyal-h <72493321+hfg-nyal-h@users.noreply.github.com>
2021-01-26 23:17:48 +0100

    button enable

--- tic-tac-toe/tictactoescript.js
+++ tic-tac-toe/tictactoescript.js
@@ -6,10 +6,27 @@ let buttonResetDiv =document.querySelector(".resetButton");
 //console.log(werIstDranDiv);
 //console.log(buttonResetDiv);
 
-let currentPlayer = "url('circle.png')";
+let random = Math.random()
 
 const playerX= "url('cancel.png')";
 const playerO= "url('circle.png')";
+
+let gameStatus = false;
+
+let currentPlayer = [];
+
+
+if (random > 0.5) {
+    
+currentPlayer = playerX;
+$(".werIstDran").text("Spieler X ist dran")
+
+} else if (random <= 0.5) {
+ 
+currentPlayer = playerO;
+$(".werIstDran").text("Spieler O ist dran")
+
+}
 
 //Es wurde KEIN Spielfeld generiert
 let layoutUpdate = false;
@@ -18,19 +35,24 @@ let layoutUpdate = false;
 let spielfeldNumber = [];
 //Hier wird die Größe des Spielfelds angegeben (n x n)
 
+
+
 function generateSpielfeld() {
 
   //Die Zahl die im Eingabefeld Input steht wird verwendet
   spielfeldNumber = Number(document.getElementById("input").value);
-    //herunterrechnen, neue Felder minus die vorhandenen
-    /*
-    for (let i = 0; i < (spielfeldNumber * spielfeldNumber-9); i++) {
-    // Erstelle ein div container mit der Klasse gitter
-    const Feld = $("<div></div>").appendTo(".spielfeld").addClass("gitter")
-    console.log("so viele divs")
-    }
-*/
-//let j = 1;
+
+//Wenn Spiel im gange ist Button disablen
+  if (gameStatus === true) { 
+
+    $("#btnGenerate").attr("disabled", true).click(function(){
+      alert("Das Spielfeld kann nur ein mal generiert werden und nur dann, wenn das Spiel nicht am laufen ist! Drücke den Reset button um das Spiel zurückzsuetzen")
+    });
+
+    //Wenn nicht dann Spielfeld enerieren
+    } else {
+
+
 $('.spielfeld').empty();
 
     for (let i = 0; i < spielfeldNumber; i++) {
@@ -44,36 +66,36 @@ $('.spielfeld').empty();
     }
 
   //Änder das Stylesheet des spielfelds so ab, dass die neue Anzahl an felder angepasst wird
-  const layout = $(".spielfeld").css( "grid-template-columns", "repeat(" + spielfeldNumber + ", 1fr)" );
+ $(".spielfeld").css( "grid-template-columns", "repeat(" + spielfeldNumber + ", 1fr)" );
   // Es wurde ein Spielfeld generiert
   layoutUpdate = true;
  // console.log(layoutUpdate); 
-
+  }
 }
 
-let numItems = $('.spielfeld').children('div').length;
 
-//console.log(numItems);
+//let numItems = $('.spielfeld').children('div').length;
 
-  // Wechsel der Spieler pro click, mit schleife?
-  // Maximale Spielzüge aus value oder aus anzahl der gitter divs
 
-  //function aufrufen (Beginn der Schleife mit erstem Click auf ein Feld -> anfangs jedoch mit Button Start)
-  //Schleife die definiert wie oft der Prozess sich wiederholen soll. i ist in dem Falle die Anzahl der Divs die zur verfügung stehen
-    // schleife die 1 addiert
-        //jquery befehl dass ich beim clicken ein neues Feld clicken kann
-    //schleife die 1 wieder subtrahiert
-//und wieder zum anfang der schleife
-
+// Abwechselnde Spielzüge
 
 $(".spielfeld").on("click", "div", function(){
-  $(this).css("background",currentPlayer);
+  $(this).css({"background": currentPlayer,  // set background to currentPlayer X oder currentPlayer O
+              "pointer-events" : "none",    // unclickbar (wegen no pointer event) nachdem geclickt
+});
 if (currentPlayer === playerO) {
 
 currentPlayer = playerX ;
+$(".werIstDran").text("Spieler X ist dran")
+//Spiel ist im gange
+gameStatus = true;
+console.log(gameStatus)
  
 }else if (currentPlayer === playerX){
 
 currentPlayer = playerO;
+$(".werIstDran").text("Spieler O ist dran")
+//Spiel ist im gange
+gameStatus = true;
 }
 })
