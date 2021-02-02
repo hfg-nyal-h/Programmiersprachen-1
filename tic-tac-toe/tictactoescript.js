466fecc8b5bcd85d6134c0eb7c1fd6e42e6bd753
hfg-nyal-h <72493321+hfg-nyal-h@users.noreply.github.com>
2021-02-02 12:54:00 +0100

    Gewinnerkennung start

--- tic-tac-toe/tictactoescript.js
+++ tic-tac-toe/tictactoescript.js
@@ -1,55 +1,45 @@
-//HTML Elements
-
-let werIstDranDiv = document.querySelector(".werIstDran");
-let buttonResetDiv =document.querySelector(".resetButton");
-
-//console.log(werIstDranDiv);
-//console.log(buttonResetDiv);
-
-let random = Math.random()
-
-const playerX= "url('cancel.png')";
-const playerO= "url('circle.png')";
+//HTML Verknüpfungen
 
 let spielerEins = document.getElementById("inputEins").value;
 let spielerZwei = document.getElementById("inputZwei").value;
-
-let gameStatus = false;
-
-let currentPlayer = [];
-
 const btnGenerate = document.getElementById("btnGenerate");
 
+//variablen
+let random = Math.random()
+let gameStatus = false;
+let currentPlayer = [];
+let layoutUpdate = false; //Es wurde KEIN Spielfeld generiert
+let spielfeldNumber = [];//Platzhalter zum generieren der Spielfeldgröße
+
+//constanten
 
 
 
 
+const playerX= "x";
+const playerO= "o";
 
-//Zufall wer als erstes dran kommt
+
+
+//Zufällig generieren wer Beginnt
 if (random > 0.5) {
     
 currentPlayer = playerX;
 $(".werIstDran").text(spielerEins+" ist dran")
 
 } else if (random <= 0.5) {
- 
-currentPlayer = playerO;
-$(".werIstDran").text(spielerZwei+ " ist dran")
-
+  currentPlayer = playerO;
+  $(".werIstDran").text(spielerZwei+ " ist dran")
 }
 
-//Es wurde KEIN Spielfeld generiert
-let layoutUpdate = false;
 
-//Platzhalter zum generieren der Spielfeldgröße
-let spielfeldNumber = [];
+
 //Hier wird die Größe des Spielfelds angegeben (n x n)
-
 
 btnGenerate.addEventListener("click", function(){
 
   //Die Zahl die im Eingabefeld Input steht wird verwendet
-  spielfeldNumber = Number(document.getElementById("input").value);
+let  spielfeldNumber = Number(document.getElementById("input").value);
 
 //Wenn Spiel im gange ist Button disablen
   if (gameStatus === true) { 
@@ -59,7 +49,7 @@ btnGenerate.addEventListener("click", function(){
     });
 
     //Wenn nicht dann Spielfeld enerieren
-    } else {
+  } else {
 
 
 $('.spielfeld').empty();
@@ -89,8 +79,8 @@ $('.spielfeld').empty();
 // Abwechselnde Spielzüge
 
 $(".spielfeld").on("click", "div", function(){
-  $(this).css({"background": currentPlayer,  // set background to currentPlayer X oder currentPlayer O
-              "pointer-events" : "none",    // unclickbar (wegen no pointer event) nachdem geclickt
+  this.classList.add(currentPlayer), //bei click klasse "x" oder "o" hinzufügen
+  $(this).css({"pointer-events" : "none",    // unclickbar (wegen no pointer event) nachdem geclickt
 });
 if (currentPlayer === playerO) {
 
@@ -99,6 +89,7 @@ $(".werIstDran").text(spielerEins+" ist dran")
 //Spiel ist im gange
 gameStatus = true;
 console.log(gameStatus)
+checkWinner();
  
 }else if (currentPlayer === playerX){
 
@@ -106,6 +97,7 @@ currentPlayer = playerO;
 $(".werIstDran").text(spielerZwei+" ist dran")
 //Spiel ist im gange
 gameStatus = true;
+checkWinner();
 }
 })
 
@@ -114,15 +106,14 @@ gameStatus = true;
 $("#btnReset").click(function(){
 
 $("#spielfeld").children("div").css({
-      "background": "black",  // set background to currentPlayer X oder currentPlayer O
+      //"background": "black",  // set background to currentPlayer X oder currentPlayer O
       "pointer-events" : "all",
-})
+}).removeClass([ "x", "o" ]) //entfernt Klassen bei reset
 gameStatus = false;
 });
 
 
 //Spielername Input
-
 
 $("#inputEins").on('input', function () {
    spielerEins = document.getElementById("inputEins").value;
@@ -131,3 +122,34 @@ $("#inputEins").on('input', function () {
 $("#inputZwei").on('input', function () {
   spielerZwei = document.getElementById("inputZwei").value;
 });
+
+//Gewinnerkennung
+
+
+//let divNumber = [];
+
+/*
+//schleife größe der Gewinnerkennung
+for (let g = 0; g < (Number(document.getElementById("input").value) * Number(document.getElementById("input").value)+1); g++) {
+  console.log(divNumber);
+   divNumber = g
+
+}
+*/
+function checkWinner(){
+
+const gitterDivs = document.querySelectorAll('.gitter');
+
+
+const Zeile0Spalte0 = gitterDivs[0].classList.item(1);
+const Zeile1Spalte0 = gitterDivs[1].classList.item(1);
+const Zeile2Spalte0 = gitterDivs[2].classList.item(1);
+
+console.log(Zeile0Spalte0);
+console.log(Zeile1Spalte0);
+console.log(Zeile2Spalte0);
+
+if ((Zeile0Spalte0 && Zeile0Spalte0 === Zeile1Spalte0 && Zeile0Spalte0 === Zeile2Spalte0)) {
+  console.log ("xo hat gewonnen")
+}
+}
